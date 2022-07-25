e24a27df537b37226ce6e16be828fc299264157f
SeNicko <sebastianflajszer@gmail.com>
2022-07-25 21:15:56 +0200

    Copy some examples from getting started

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,44 @@
-#[macro_use]
-extern crate rocket;
+use std::sync::Mutex;
 
-#[macro_use]
-extern crate diesel;
+use actix_web::{get, web, App, HttpServer};
 
-extern crate dotenv;
+struct AppState {
+    app_name: String,
+}
 
-mod product;
-mod schema;
+#[get("/welcome")]
+async fn hello(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name;
+    format!("Welcome to {app_name}!")
+}
 
-use rocket::{Build, Rocket};
+struct AppStateWithCounter {
+    counter: Mutex<i32>,
+}
 
-#[launch]
-fn rocket() -> Rocket<Build> {
-    let connection = rustmerce::establish_connection();
-    let products_dao = product::ProductImpl::new(connection);
+async fn index(data: web::Data<AppStateWithCounter>) -> String {
+    let mut counter = data.counter.lock().unwrap();
+    *counter += 1;
 
-    rocket::build()
-        .attach(product::stage())
-        .manage(products_dao)
+    format!("Request number: {counter}")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let counter = web::Data::new(AppStateWithCounter {
+        counter: Mutex::new(0),
+    });
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(counter.clone())
+            .app_data(web::Data::new(AppState {
+                app_name: String::from("Actix Web"),
+            }))
+            .route("/", web::get().to(index))
+            .service(hello)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
