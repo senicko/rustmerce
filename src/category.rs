dae79e23d157c02a44c78dfd45272989196f3e8b
SeNicko <sebastianflajszer@gmail.com>
2022-08-30 10:35:01 +0200

    Return tree of categories (parent->children->..) when getting all categories

--- src/category.rs
+++ src/category.rs
@@ -1,13 +1,26 @@
 use serde::{Deserialize, Serialize};
-use tokio_pg_mapper_derive::PostgresMapper;
+use tokio_postgres::Row;
 
 pub mod handlers;
 pub mod store;
 
-#[derive(Serialize, Deserialize, PostgresMapper)]
-#[pg_mapper(table = "categories")]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct Category {
     pub id: i32,
     pub name: String,
     pub parent_id: Option<i32>,
+    pub children: Vec<Category>,
+}
+
+impl TryFrom<&Row> for Category {
+    type Error = tokio_pg_mapper::Error;
+
+    fn try_from(row: &Row) -> Result<Self, Self::Error> {
+        Ok(Category {
+            id: row.try_get("id")?,
+            name: row.try_get("name")?,
+            parent_id: row.try_get("parent_id")?,
+            children: Vec::new(),
+        })
+    }
 }
--- src/category/store.rs
+++ src/category/store.rs
@@ -1,6 +1,6 @@
-use super::Category;
 use deadpool_postgres::Pool;
-use tokio_pg_mapper::FromTokioPostgresRow;
+
+use super::Category;
 
 #[derive(thiserror::Error, Debug)]
 pub enum CategoryStoreError {
@@ -24,15 +24,53 @@ impl CategoryStore {
         Self { db_pool }
     }
 
+    // group_categories groups categories together.
+    // TODO: There is a lot of cloning. Maybe there is a way to avoid it?
+    fn group_categories(&self, id: Option<i32>, categories: Vec<Category>) -> Vec<Category> {
+        let mut parent_categories = categories
+            .iter()
+            .cloned()
+            .filter(|c| c.parent_id == id)
+            .collect::<Vec<Category>>();
+
+        parent_categories.iter_mut().for_each(|c| {
+            let children = self.group_categories(Some(c.id), categories.clone());
+            c.children = children;
+        });
+
+        parent_categories
+    }
+
     pub async fn get_all(&self) -> Result<Vec<Category>, CategoryStoreError> {
         let conn = self.db_pool.get().await?;
 
-        let rows = conn
-            .query("SELECT * FROM categories WHERE parent_id IS NULL", &[])
-            .await?;
+        let rows = conn.query("SELECT * FROM categories", &[]).await?;
 
-        rows.iter()
-            .map(|row| Ok(Category::from_row_ref(row)?))
-            .collect()
+        let all_categories = rows
+            .iter()
+            .map(|row| Ok(Category::try_from(row)?))
+            .collect::<Result<Vec<Category>, CategoryStoreError>>()?;
+
+        Ok(self.group_categories(None, all_categories))
     }
+
+    // WIP
+    // pub async fn get_one(&self, id: i32) -> Result<Category, CategoryStoreError> {
+    //     let conn = self.db_pool.get().await?;
+
+    //     let rows = conn
+    //         .query(
+    //             "SELECT c.* FROM categories as c, get_category_tree(1) as t WHERE c.id IN (t)",
+    //             &[&id],
+    //         )
+    //         .await?;
+
+    //     let category =
+    //         Category::try_from(rows.get(0).ok_or(CategoryStoreError::MappingFailed(()))?)?;
+
+    //     let mut category = Category::try_from(row)?;
+    //     category.children = self.group_categories(id).await?;
+
+    //     Ok(category)
+    // }
 }
