c7f6afbab3445d79e2bd6de1a6f586c5a0605594
senicko <sebastianflajszer@gmail.com>
2022-09-15 18:06:40 +0200

    Add simple redis integration

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,41 @@ use actix_cors::Cors;
 use actix_web::{middleware::Logger, web, App, HttpServer};
 use deadpool_postgres::{Config, ManagerConfig, Pool, RecyclingMethod, Runtime};
 use dotenv::dotenv;
+use redis::Commands;
 use std::env;
 use tokio_postgres::NoTls;
+
+extern crate redis;
 
 mod category;
 mod product;
 mod storage;
 
+fn init_redis_connection() -> redis::Connection {
+    let client = redis::Client::open("redis://127.0.0.1/").expect("Invalid redis connection url");
+
+    client
+        .get_connection()
+        .expect("Failed to connect with redis")
+}
+
+fn test_redis() {
+    let mut redis_conn = init_redis_connection();
+
+    // throw away the result, just make sure it does not fail
+    let _: () = redis_conn.set("my_key", 42).unwrap();
+
+    // read back the key and return it.  Because the return value
+    // from the function is a result for integer this will automatically
+    // convert into one.
+    let value: i32 = redis_conn.get("my_key").unwrap();
+
+    println!("{value}");
+}
+
 fn init_db_pool() -> Pool {
-    let db_username = env::var("DB_USERNAME").expect("DB_USERNAME isn't set");
-    let db_url = env::var("DB_NAME").expect("DB_NAME isn't set");
+    let db_username = env::var("DB_USERNAME").expect("DB_USERNAME enviroment variable missing");
+    let db_url = env::var("DB_NAME").expect("DB_NAME enviroment variable missing");
 
     let mut config = Config::new();
 
@@ -39,6 +64,8 @@ async fn main() -> std::io::Result<()> {
     std::fs::create_dir_all("./assets").expect("Failed to create ./assets");
 
     let db_pool = init_db_pool();
+
+    test_redis();
 
     let product_store = product::store::ProductStore::new(db_pool.clone());
     let category_store = category::store::CategoryStore::new(db_pool.clone());
